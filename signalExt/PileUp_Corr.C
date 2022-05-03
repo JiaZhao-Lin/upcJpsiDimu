b14f61eeb5c85b76eb3a1bb3b4b753a408d90723
JiaZhao Lin <40001102+JiaZhao-Lin@users.noreply.github.com>
2022-05-03 14:58:25 -0500

    Added Shuai's PU Method Back
    cleared the git cache and deleted plots in physicsFigure/outplots/
    Updated parameters in Physparameters.h

--- signalExt/PileUp_Corr.C
+++ signalExt/PileUp_Corr.C
@@ -1,13 +1,13 @@
 
-const int nDmatrixs = 3; 
+// const int nDmatrixs = 3; 
+const int nDmatrixs = 4; //For Shuai's method
 TMatrixD MigrationMatrix(nDmatrixs,nDmatrixs);
 TMatrixD MigrationMatrixInvert(nDmatrixs,nDmatrixs);
 
-const double f_00	=	0.889779, f_0X	=	(0.053063600+0.050845800)/2,	f_XX	=	0.0014546+0.00140865+0.00150574+0.0019427;
-std::vector<double> f_v{f_00, f_0X,	f_XX};
-
 double Cal_Chi2(const double Ps, const double Pm)
 {
+	const double f_00	=	0.889779, f_0X	=	(0.053063600+0.050845800)/2,	f_XX	=	0.0014546+0.00140865+0.00150574+0.0019427;
+	std::vector<double> f_v{f_00, f_0X,	f_XX};
 	std::vector<double> f_Err_v;
 	TVector3	f_ZB	=	{f_00,	f_0X,	f_XX};
 	double nDen = 1e6; double nDenErr = sqrt(nDen);
@@ -32,6 +32,7 @@ double Cal_Chi2(const double Ps, const double Pm)
 
 double Cal_Squares(const double Ps, const double Pm)
 {
+	const double f_00	=	0.889779, f_0X	=	(0.053063600+0.050845800)/2,	f_XX	=	0.0014546+0.00140865+0.00150574+0.0019427;
 	TVector3	f_ZB	=	{f_00,	f_0X,	f_XX};
 
 	double A = pow( (1 - Ps), 2) * (1-Pm);
@@ -80,12 +81,32 @@ void getInteractionProbability()
 
 TMatrixD getPileUp_CorrFactor()
 {
+	//---------------------------------------ATLAST Correction Method----------------------------------------
 	// const double Ps = 0.0559; const double Pm = 0.00174;	//ATLAST Number
-	const double Ps = 0.0552; const double Pm = 0.00325;	//From CMS ZB Data with Least Square Method
+	// const double Ps = 0.0552; const double Pm = 0.00325;	//From CMS ZB Data with Least Square Method
 
-	const double MigrationMatrixElement[nDmatrixs*nDmatrixs] = {	(1-Ps)*(1-Ps)*(1-Pm)	,		0 				,	0,
-											   						2*Ps*(1-Ps)*(1-Pm)		,		(1-Ps)*(1-Pm)	,	0,
-											   						Pm+Ps*Ps*(1-Pm)			,		Pm+Ps*(1-Pm)	,	1};
+	// const double MigrationMatrixElement[nDmatrixs*nDmatrixs] = {	(1-Ps)*(1-Ps)*(1-Pm)	,		0 				,	0,
+	// 										   						2*Ps*(1-Ps)*(1-Pm)		,		(1-Ps)*(1-Pm)	,	0,
+	// 										   						Pm+Ps*Ps*(1-Pm)			,		Pm+Ps*(1-Pm)	,	1};
+	//-------------------------------------------------------------------------------------------------------
+
+	//---------------------------------------Yang Shuai Correction Method------------------------------------
+	const double Pzb00 = 0.889779;
+	const double Pzb0X = 0.0275305+0.0255331;
+	const double PzbX0 = 0.0256458+0.0252;
+	const double PzbXX = 0.00150574+0.0019427+0.0014546+0.00140865;
+
+	const double P00_00 = Pzb00;
+	const double P0X_00 = Pzb0X; const double P0X_0X = Pzb00 + Pzb0X;
+	const double PX0_00 = PzbX0; const double PX0_X0 = Pzb00 + PzbX0;
+	const double PXX_00 = PzbXX; const double PXX_0X = PzbX0 + PzbXX; const double PXX_X0 = Pzb0X + PzbXX;
+	const double PXX_XX = 1.0;
+
+	const double MigrationMatrixElement[nDmatrixs*nDmatrixs] = {	P00_00,	0,		0,		0,
+											   						P0X_00,	P0X_0X,	0,		0,
+											   						PX0_00,	0,		PX0_X0,	0,
+											   						PXX_00,	PXX_0X,	PXX_X0,	PXX_XX };
+	//-------------------------------------------------------------------------------------------------------
 
 	MigrationMatrix       = TMatrixD(nDmatrixs,	nDmatrixs,	MigrationMatrixElement);
 	MigrationMatrixInvert = MigrationMatrix; MigrationMatrixInvert.Invert();
@@ -110,39 +131,76 @@ void PileUp_Corr( double NJpsi_inMFit[][7], double NerrJpsi_inMFit[][7], const d
 
 	for (int iy = 0; iy < nDiffRapBins; ++iy)
 	{
-		double tem[nDmatrixs] = 
-		{
-			NJpsi_inMFit[1][iy],
-			NJpsi_inMFit[4][iy],
-			NJpsi_inMFit[5][iy]
-		};
+		//---------------------------------------ATLAST Correction Method----------------------------------------
+		// double tem[nDmatrixs] = 
+		// {
+		// 	NJpsi_inMFit[1][iy],
+		// 	NJpsi_inMFit[4][iy],
+		// 	NJpsi_inMFit[5][iy]
+		// };
 
+		// TMatrixD N_Meastured_iy(nDmatrixs,	1, tem); 
+		// TMatrixD N_True_iy = MigrationMatrixInvert * N_Meastured_iy;
+
+		// cout<<"N_True_iy.GetNrows(): "<<N_True_iy.GetNrows()<<endl;
+		// cout<<"N_True_iy.GetNcols(): "<<N_True_iy.GetNcols()<<endl;
+		
+		// NJpsi_inMFit[1][iy] = N_True_iy[0][0];
+		// NJpsi_inMFit[4][iy] = N_True_iy[1][0];
+		// NJpsi_inMFit[5][iy] = N_True_iy[2][0];
+
+		// auto delta0 = sqrt( pow(MigrationMatrixInvert[0][0] * NerrJpsi_inMFit[1][iy], 2) +
+		// 					pow(MigrationMatrixInvert[0][1] * NerrJpsi_inMFit[4][iy], 2) +
+		// 					pow(MigrationMatrixInvert[0][2] * NerrJpsi_inMFit[5][iy], 2) );
+
+		// auto delta1 = sqrt( pow(MigrationMatrixInvert[1][0] * NerrJpsi_inMFit[1][iy], 2) +
+		// 					pow(MigrationMatrixInvert[1][1] * NerrJpsi_inMFit[4][iy], 2) +
+		// 					pow(MigrationMatrixInvert[1][2] * NerrJpsi_inMFit[5][iy], 2) );
+		
+		// auto delta2 = sqrt( pow(MigrationMatrixInvert[2][0] * NerrJpsi_inMFit[1][iy], 2) +
+		// 					pow(MigrationMatrixInvert[2][1] * NerrJpsi_inMFit[4][iy], 2) +
+		// 					pow(MigrationMatrixInvert[2][2] * NerrJpsi_inMFit[5][iy], 2) );
+		
+		// NerrJpsi_inMFit[1][iy] = delta0;
+		// NerrJpsi_inMFit[4][iy] = delta1;
+		// NerrJpsi_inMFit[5][iy] = delta2;
+		//-------------------------------------------------------------------------------------------------------
+
+		//---------------------------------------Yang Shuai Correction Method------------------------------------
+		double tem[nDmatrixs] = {NJpsi_inMFit[1][iy],	NJpsi_inMFit[2][iy],	NJpsi_inMFit[3][iy],	NJpsi_inMFit[5][iy]};
 		TMatrixD N_Meastured_iy(nDmatrixs,	1, tem); 
 		TMatrixD N_True_iy = MigrationMatrixInvert * N_Meastured_iy;
 
-		cout<<"N_True_iy.GetNrows(): "<<N_True_iy.GetNrows()<<endl;
-		cout<<"N_True_iy.GetNcols(): "<<N_True_iy.GetNcols()<<endl;
-		
 		NJpsi_inMFit[1][iy] = N_True_iy[0][0];
-		NJpsi_inMFit[4][iy] = N_True_iy[1][0];
-		NJpsi_inMFit[5][iy] = N_True_iy[2][0];
+		NJpsi_inMFit[2][iy] = N_True_iy[1][0];
+		NJpsi_inMFit[3][iy] = N_True_iy[2][0];
+		NJpsi_inMFit[5][iy] = N_True_iy[3][0];
 
-		auto delta0 = sqrt( pow(MigrationMatrixInvert[0][0] * NerrJpsi_inMFit[1][iy], 2) +
-							pow(MigrationMatrixInvert[0][1] * NerrJpsi_inMFit[4][iy], 2) +
-							pow(MigrationMatrixInvert[0][2] * NerrJpsi_inMFit[5][iy], 2) );
+		NJpsi_inMFit[4][iy] = N_True_iy[1][0] + N_True_iy[2][0];
 
-		auto delta1 = sqrt( pow(MigrationMatrixInvert[1][0] * NerrJpsi_inMFit[1][iy], 2) +
-							pow(MigrationMatrixInvert[1][1] * NerrJpsi_inMFit[4][iy], 2) +
-							pow(MigrationMatrixInvert[1][2] * NerrJpsi_inMFit[5][iy], 2) );
-		
-		auto delta2 = sqrt( pow(MigrationMatrixInvert[2][0] * NerrJpsi_inMFit[1][iy], 2) +
-							pow(MigrationMatrixInvert[2][1] * NerrJpsi_inMFit[4][iy], 2) +
-							pow(MigrationMatrixInvert[2][2] * NerrJpsi_inMFit[5][iy], 2) );
-		
+		auto delta0 = sqrt( pow(MigrationMatrixInvert[0][0] * NerrJpsi_inMFit[1][iy],2) + 
+							pow(MigrationMatrixInvert[0][1] * NerrJpsi_inMFit[2][iy],2) + 
+							pow(MigrationMatrixInvert[0][2] * NerrJpsi_inMFit[3][iy],2) + 
+							pow(MigrationMatrixInvert[0][3] * NerrJpsi_inMFit[5][iy],2) );
+		auto delta1 = sqrt( pow(MigrationMatrixInvert[1][0] * NerrJpsi_inMFit[1][iy],2) + 
+							pow(MigrationMatrixInvert[1][1] * NerrJpsi_inMFit[2][iy],2) + 
+							pow(MigrationMatrixInvert[1][2] * NerrJpsi_inMFit[3][iy],2) + 
+							pow(MigrationMatrixInvert[1][3] * NerrJpsi_inMFit[5][iy],2) );
+		auto delta2 = sqrt( pow(MigrationMatrixInvert[2][0] * NerrJpsi_inMFit[1][iy],2) + 
+							pow(MigrationMatrixInvert[2][1] * NerrJpsi_inMFit[2][iy],2) + 
+							pow(MigrationMatrixInvert[2][2] * NerrJpsi_inMFit[3][iy],2) + 
+							pow(MigrationMatrixInvert[2][3] * NerrJpsi_inMFit[5][iy],2) );
+		auto delta3 = sqrt( pow(MigrationMatrixInvert[3][0] * NerrJpsi_inMFit[1][iy],2) + 
+							pow(MigrationMatrixInvert[3][1] * NerrJpsi_inMFit[2][iy],2) + 
+							pow(MigrationMatrixInvert[3][2] * NerrJpsi_inMFit[3][iy],2) + 
+							pow(MigrationMatrixInvert[3][3] * NerrJpsi_inMFit[5][iy],2) );
 		NerrJpsi_inMFit[1][iy] = delta0;
-		NerrJpsi_inMFit[4][iy] = delta1;
-		NerrJpsi_inMFit[5][iy] = delta2;
-		
+		NerrJpsi_inMFit[2][iy] = delta1;
+		NerrJpsi_inMFit[3][iy] = delta2;
+		NerrJpsi_inMFit[5][iy] = delta3;
+
+		NerrJpsi_inMFit[4][iy] = sqrt( pow(NerrJpsi_inMFit[2][iy],2) + pow(NerrJpsi_inMFit[3][iy],2) );
+		//-------------------------------------------------------------------------------------------------------
 	}
 }
 
